8e372f9f69ae6d8ef998f5dedc7e4442a97857e6
Lada Kolotova <ladakolotova@MacBook-Pro-3.local>
2024-04-27 13:41:05 +0300

    Backend

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django')
 
 DEBUG = os.getenv('DEBUG', True)
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'kittygram-ladank.sytes.net')
 
 INSTALLED_APPS = [
     'django.contrib.admin',

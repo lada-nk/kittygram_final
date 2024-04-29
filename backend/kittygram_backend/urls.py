f873d601dd908218896d0aa12aa94d0aaa7ab24e
Lada Kolotova <ladakolotova@MacBook-Pro-3.local>
2024-04-29 12:42:00 +0300

    Main

--- backend/kittygram_backend/urls.py
+++ backend/kittygram_backend/urls.py
@@ -4,25 +4,17 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
-
+ 
 router = routers.DefaultRouter()
 router.register(r'cats', CatViewSet)
 router.register(r'achievements', AchievementViewSet)
 
-api_urls = [
-    path('', include(router.urls)),
-    path('', include('djoser.urls')),  # Работа с пользователями
-    path('', include('djoser.urls.authtoken')),  # Работа с токенами
-]
-
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('api/', include(api_urls)),
     path('api/', include(router.urls)),
     path('api/', include('djoser.urls')),  # Работа с пользователями
     path('api/', include('djoser.urls.authtoken')),  # Работа с токенами
-]
+] 
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
